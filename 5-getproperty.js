0ec88e6db013c8ad0b870c851ea18040edea1072
dani <danieljuhe@gmail.com>
2022-10-21 18:14:03 +0200

    instrucciones ejercicio 5

---
+++ 5-getproperty.js
@@ -0,0 +1,7 @@
+function getProperty(obj, key) {
+    return obj, key;
+  }
+  let output=getProperty();
+  console.log(output);
+  
+  //Si no hay una propiedad en esa key, debería retornar undefined.
